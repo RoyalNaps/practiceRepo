ed814cb29bd6e56cf7ca377fc9b972b4a3fa46e2
Collin Mullis <rnapscode@gmail.com>
2016-01-05 18:56:57 -0500

    Added name and time stamp to code.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,4 +1,8 @@
-
+/* Collin Mullis
+ * Coded January 1st 2016
+ * Hello World App
+ * "Write an App that will print out, "Hello, World" when complied and ran."
+ */
 public class HelloWorld {
 
 	public static void main(String[] args) {
